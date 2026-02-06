c41f2543bc7513e7e28e4ea169e3a10dbfeeac64
Affan Nabeel <affan@technassolution.com>
2026-02-06 10:11:36 +0530

    Minor CHange

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,6 @@
-const API_URL = "https://notes-sk00.onrender.com/api";
+const API_URL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:5000/api"
+        : "https://notes-sk00.onrender.com/api";
+
 export default API_URL;
